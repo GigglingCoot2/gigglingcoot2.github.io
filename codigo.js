8ba2dec2bd8ee82a5c21de778782ae57c1b890ea
Germán Pancardo <91687146+gpancardo@users.noreply.github.com>
2024-03-14 00:21:08 -0600

    ?

--- codigo.js
+++ codigo.js
@@ -80,7 +80,6 @@ function mostrarNivel(){
             document.getElementById("img").src="";
             document.getElementById("boton0").style.display = 'none';
             document.getElementById("boton1").style.display = 'none';
-            document.getElementById("inventario").style.display = 'none';
         break;
     }
     document.getElementById("inventario").innerHTML=("Inventario: "+inventario);
